a5bcab9e259e1b99ac9e146ded70456a5e86d017
Mariyan Georgiev <mariqn5000@gmail.com>
2022-11-06 02:11:16 +0200

    fixes

--- Octomber_Hackathon/settings.py
+++ Octomber_Hackathon/settings.py
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'Octomber_Hackathon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'october_hackathon1',
-        'USER': 'postgres',
-        'PASSWORD': 'Thatshurt',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'october_hackathon1',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Thatshurt',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
